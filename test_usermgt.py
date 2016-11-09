3a259de4cb9616617f8ecf2beb0264999efa5d02
MelisaGP <melisagp47@gmail.com>
2016-11-08 20:56:28 -0500

    Update test_usermgt.py

--- test_usermgt.py
+++ test_usermgt.py
@@ -10,10 +10,7 @@ def test_get_all_files(client):
 	result = client.get('/v1.0/files',follow_redirects=True)
 	assert b'files' in result.data
 	
-def test_post_files(client):
-  parcial_2  = {'file': 'parcial_2', 'content': 'Parcial Melisa'}
-  response = client.post('/v1.0/files',data = parcial_2)
-  assert "HTTP 201 CREATE"
+
 
 
 
